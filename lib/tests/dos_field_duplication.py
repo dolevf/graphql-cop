d4f48507eb37d17a9b0e8febb4688ca4965742b1
Dolev Farhi <farhi.dolev@gmail.com>
2022-03-04 15:46:17 -0500

    Update dos_field_duplication.py

--- lib/tests/dos_field_duplication.py
+++ lib/tests/dos_field_duplication.py
@@ -3,7 +3,7 @@ from lib.utils import graph_query
 def field_duplication(url):
   result = False
 
-  duplicated_string = '__typename \n' * 10000
+  duplicated_string = '__typename \n' * 1000
   q = 'query { ' + duplicated_string + '} '
   gql_response = graph_query(url, payload=q)
   try:
